fe09d37fbfa30dfa106713c6d23881495824d436
edilberto-pajunar <espajunarjr@gmail.com>
2026-02-05 19:00:14 +0800

    fix issue

--- app/dashboard/components/RecentActivity.tsx
+++ app/dashboard/components/RecentActivity.tsx
@@ -7,7 +7,7 @@ interface RecentActivityProps {
 
 export default function RecentActivity({
     log
-} : RecentActivityProps) {
+}: RecentActivityProps) {
 
     const levelColor = () => {
         switch (log.level) {
@@ -27,7 +27,7 @@ export default function RecentActivity({
             <div className="w-2 h-2 rounded-full mt-1.5 shrink-0" style={{ backgroundColor: levelColor() }}></div>
             <div className="flex-1 min-w-0">
                 {/* <p className="text-sm font-medium leading-none mb-1" style={{ color: COFFEE_PALETTE.textPrimary }}>{log.msg}</p> */}
-                <p className="text-[10px]" style={{ color: COFFEE_PALETTE.textSecondary }}>{log.id} • {log.time.toLocaleString()}</p>
+                <p className="text-[10px]" style={{ color: COFFEE_PALETTE.textSecondary }}>{log.id} • {log.timestamp.toLocaleString()}</p>
             </div>
         </div>
     );
